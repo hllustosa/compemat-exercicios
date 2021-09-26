e0c49a4da4ddfd300d2ab19107be65bb87d47a91
Hermano <hermano_lustosa@hotmail.com>
2021-09-26 11:18:01 -0300

    :tada: Initial commit

---
+++ seção 1/solução-produto-simples.js
@@ -0,0 +1,9 @@
+//Fazendo a entrada das variáveis x e y
+var x = input('x');
+var y = input('y');
+
+//Calculando o produto das variáveis
+var r = x * y;
+
+//Fazendo a saída do resultado contido em r
+output(r)
