0adc21645636241b617626fed13a38ca7482ed2e
Hermano <hermano_lustosa@hotmail.com>
2022-01-18 10:24:20 -0300

    :sparkles: Fixes solution input

--- unidade 5/ex_jovens_trabalhadores.js
+++ unidade 5/ex_jovens_trabalhadores.js
@@ -1,5 +1,5 @@
 var somente_estuda = input("somente_estuda");
-var trabalhao_estuda = input("trabalhao_estuda");
+var trabalha_estuda = input("trabalha_estuda");
 var somente_trabalha = input("somente_trabalha");
 var nao_trabalha_nao_estuda = input("nao_trabalha_nao_estuda");
 
@@ -7,7 +7,7 @@ var nao_trabalha_nao_estuda = input("nao_trabalha_nao_estuda");
 var total_pesquisa = 363000;
 
 //Calculo dos total de jovens que trabalham
-var total = (total_pesquisa * (trabalhao_estuda + somente_trabalha)) / 100.0;
+var total = (total_pesquisa * (trabalha_estuda + somente_trabalha)) / 100.0;
 
 //Realizando a saída
 output(total);
